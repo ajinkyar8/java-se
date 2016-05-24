870d99ec47c39919f03579d0f38d78efab26b445
ajinkyar8 <ajinkya.r@gmail.com>
2016-05-24 05:46:55 +0530

    Add files via upload

---
+++ FileDemo.java
@@ -0,0 +1,15 @@
+import java.io.File;
+
+
+public class FileDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		File file = new File("D:\\AJ");
+		System.out.println(file.getAbsolutePath());
+	}
+
+}
